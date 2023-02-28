f8cf1ab4ab3af06e165494783da311af2fe7108d
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-02-28 11:54:17 +0100

    debut homepage les boys🤣

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/home', name: 'home_')]
 class HomePageController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('home_page/index.html.twig', [
