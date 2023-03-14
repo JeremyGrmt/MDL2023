ef965806de735e81604f167292a123039c23e448
Tibo <95851801+Tibood@users.noreply.github.com>
2023-03-14 09:08:58 +0100

    Create 404.php

---
+++ public/404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Page non trouvée</title>
+    <style>
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+      }
+      .container {
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Erreur 404: Page non trouvée</h1>
+      <p>Désolé, la page que vous recherchez est introuvable.</p>
+    </div>
+  </body>
+</html>
