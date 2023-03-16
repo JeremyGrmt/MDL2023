3d8de415acfef0aa023b1cf8a56bae7f931d58b4
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-03-16 12:48:57 +0100

    footer

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/login', name: 'login_')]
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+}
