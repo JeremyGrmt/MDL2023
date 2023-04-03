f29c54771ef25b370a837cfa343d51b28bc7a2e8
Tibo <95851801+Tibood@users.noreply.github.com>
2023-04-03 12:13:25 +0200

    Revert "debut form connection"
    This reverts commit 6da885533bddfc1f052c6d3aaf14d42572e6a08e.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,27 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Form\LoginType;
 
+#[Route('/login', name: 'login_')]
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $form = $this->createForm(LoginType::class);
-         $form->handleRequest($request);
-
         return $this->render('login/index.html.twig', [
-            'form' => $form->createView(),
             'controller_name' => 'LoginController',
         ]);
     }
-        
-    #[Route('/logout', name: 'app_logout')]
-    public function logout():void
-    {
-        
-    }
-
 }
