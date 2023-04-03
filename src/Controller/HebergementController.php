67f14d519d987ca5c2f70e9892d19244d2c48dbe
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-04-03 10:28:40 +0200

    oui

--- src/Controller/HebergementController.php
+++ src/Controller/HebergementController.php
@@ -12,8 +12,29 @@ class HebergementController extends AbstractController
     #[Route('/', name: 'app_hebergement')]
     public function index(): Response
     {
+        $formulestest = [
+            [
+                'id' => 1,
+                'titre' => 'Formule 1',
+                'description' => 'test formule 1',
+                'prix' => 19.99,
+            ],
+            [
+                'id' => 1,
+                'titre' => 'Formule 2',
+                'description' => 'test formule 2',
+                'prix' => 29.99,
+            ],
+            [
+                'id' => 1,
+                'titre' => 'Formule 3',
+                'description' => 'test formule 3',
+                'prix' => 39.99,
+            ],
+        ];
         return $this->render('hebergement/index.html.twig', [
             'controller_name' => 'HebergementController',
+            'formules' => $formulestest,
         ]);
     }
 }
