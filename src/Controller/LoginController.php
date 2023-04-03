6da885533bddfc1f052c6d3aaf14d42572e6a08e
Tibo <95851801+Tibood@users.noreply.github.com>
2023-04-03 11:52:33 +0200

    debut form connection

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,15 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\LoginType;
 
-#[Route('/login', name: 'login_')]
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(LoginType::class);
+         $form->handleRequest($request);
+
         return $this->render('login/index.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'LoginController',
         ]);
     }
+        
+    #[Route('/logout', name: 'app_logout')]
+    public function logout():void
+    {
+        
+    }
+
 }
