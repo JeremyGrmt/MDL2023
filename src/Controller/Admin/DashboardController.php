a3c99dbdf4671b2fb592c1fa0429ebc093348346
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-04-25 14:58:14 +0200

    manque plus que un truc ou 2

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         return $this->render('admin/adminboard.html.twig');
