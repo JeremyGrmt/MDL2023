e1deb11b2e1bb937fddc6edb227e0028066e8047
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-04-26 17:23:53 +0200

    hash du mdp marche pas

--- src/Controller/Admin/CompteCrudController.php
+++ src/Controller/Admin/CompteCrudController.php
@@ -3,36 +3,53 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Compte;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+#use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use App\Service\EasyAdminUserService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class CompteCrudController extends AbstractCrudController
-{
-    public static function getEntityFqcn(): string
-    {
+class CompteCrudController extends AbstractCrudController {
+
+    private $userPasswordHasher;
+
+    public function __construct(EasyAdminUserService $easyAdminUserService) {
+        $this->userPasswordHasher = $easyAdminUserService->getUserPasswordHasher();
+    }
+
+    public static function getEntityFqcn(): string {
         return Compte::class;
     }
-    
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud
-            ->setEntityLabelInPlural('Utilisateurs')
-            ->setEntityLabelInSingular('Utilisateur');
-    }
-    
-    public function configureFields(string $pageName): iterable
-    {
+
+    public function configureFields(string $pageName): iterable {
         return [
             TextField::new('email'),
-            TextField::new('password'),
-            ChoiceField::new('roles')
-                ->setChoices(['ROLE_USER' => 'ROLE_USER', 'ROLE_ADMIN' => 'ROLE_ADMIN'])
-                ->allowMultipleChoices()
-                ->renderExpanded(),
+            TextField::new('password')->hideOnIndex(),
+                    ChoiceField::new('roles')
+                    ->setChoices(['ROLE_USER' => 'ROLE_USER', 'ROLE_ADMIN' => 'ROLE_ADMIN'])
+                    ->allowMultipleChoices()
+                    ->renderExpanded(),
             BooleanField::new('isVerified'),
-            ];
+        ];
     }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void {
+        if ($entityInstance instanceof UserInterface) {
+            // encode the plain password
+            $entityInstance->setPassword(
+                    $this->userPasswordHasher->hashPassword(
+                            $entityInstance,
+                            $entityInstance->getPassword()
+                    )
+            );
+        }
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
 }
