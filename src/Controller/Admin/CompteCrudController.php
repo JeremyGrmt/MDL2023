16c828bc626b2e0b79cbd4e4fd847731f497252f
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-04-26 20:21:38 +0200

    fin de easyadmin

--- src/Controller/Admin/CompteCrudController.php
+++ src/Controller/Admin/CompteCrudController.php
@@ -13,14 +13,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class CompteCrudController extends AbstractCrudController {
-
-    private $userPasswordHasher;
-
-    public function __construct(EasyAdminUserService $easyAdminUserService) {
-        $this->userPasswordHasher = $easyAdminUserService->getUserPasswordHasher();
-    }
 
     public static function getEntityFqcn(): string {
         return Compte::class;
@@ -29,7 +26,6 @@ class CompteCrudController extends AbstractCrudController {
     public function configureFields(string $pageName): iterable {
         return [
             TextField::new('email'),
-            TextField::new('password')->hideOnIndex(),
                     ChoiceField::new('roles')
                     ->setChoices(['ROLE_USER' => 'ROLE_USER', 'ROLE_ADMIN' => 'ROLE_ADMIN'])
                     ->allowMultipleChoices()
@@ -38,18 +34,10 @@ class CompteCrudController extends AbstractCrudController {
         ];
     }
 
-    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void {
-        if ($entityInstance instanceof UserInterface) {
-            // encode the plain password
-            $entityInstance->setPassword(
-                    $this->userPasswordHasher->hashPassword(
-                            $entityInstance,
-                            $entityInstance->getPassword()
-                    )
-            );
-        }
-
-        parent::persistEntity($entityManager, $entityInstance);
+    public function configureActions(Actions $actions): Actions {
+        return $actions
+                        ->remove(Crud::PAGE_INDEX, Action::NEW)
+        ;
     }
 
 }
