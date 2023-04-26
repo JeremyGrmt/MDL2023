3e0cae1957c9e87f942eac758ee9011bc6bc5377
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2023-04-26 23:37:23 +0200

    ajout du champ chambre dans hotel

--- src/Controller/Admin/HotelCrudController.php
+++ src/Controller/Admin/HotelCrudController.php
@@ -4,22 +4,51 @@ namespace App\Controller\Admin;
 
 use App\Entity\Hotel;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
-class HotelCrudController extends AbstractCrudController
-{
-    public static function getEntityFqcn(): string
-    {
+class HotelCrudController extends AbstractCrudController {
+
+    public static function getEntityFqcn(): string {
         return Hotel::class;
     }
 
     /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+      public function configureFields(string $pageName): iterable
+      {
+      return [
+      IdField::new('id'),
+      TextField::new('title'),
+      TextEditorField::new('description'),
+      ];
+      }
+     */
+
+    public function configureFields(string $pageName): iterable {
+        $fields = [
+            TextField::new('nom'),
+            TextField::new('adresse'),
+            IntegerField::new('cp'),
+            TextField::new('ville'),
+            TextField::new('tel'),
+            EmailField::new('mail'),
+                    AssociationField::new('chambres')
+                    ->setFormTypeOption('by_reference', false)
+                    ->setFormTypeOptions([
+                        'required' => false,
+                        'multiple' => true,
+                    ]),
         ];
+
+        if ($pageName === Crud::PAGE_INDEX) {
+            // Remove the association field from the index view
+            unset($fields[count($fields) - 1]);
+        }
+
+        return $fields;
     }
-    */
+
 }
