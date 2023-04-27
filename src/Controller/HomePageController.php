9deedecedcbc7e26c87f009175ace278db69e72d
jeremyGrmt <59418445+JeremyGrmt@users.noreply.github.com>
2023-04-27 03:32:10 +0200

    front terminé page d'accueil
    ajout des fichiers SQL pour remplir la base de données
    ajout de toute la partie front de la page d'accuueil

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Atelier;
 use App\Entity\Vacation;
 use App\Entity\Theme;
@@ -10,24 +11,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 #[Route('/home', name: 'home_')]
-class HomePageController extends AbstractController
-{
+class HomePageController extends AbstractController {
+
     const DOMAINE_API = 'http://mdl-api.fr/';
-    public function index(ManagerRegistry $doctrine): Response
-    {
+
+    public function index(ManagerRegistry $doctrine): Response {
         $ateliers = $doctrine->getRepository(Atelier::class)->findAll();
-//        $hotel = $doctrine->getRepository($persistentObject)
+        $themes = $doctrine->getRepository(Theme::class)->findAll();
+        $vacations = $doctrine->getRepository(Vacation::class)->findAll();
+        $hotels = $doctrine->getRepository(Hotel::class)->findAll();
+        
         $compte = $this->getUser();
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-            'compte' => $compte,
-            'ateliers' => $ateliers
+                    'controller_name' => 'HomePageController',
+                    'compte' => $compte,
+                    'ateliers' => $ateliers,
+                    'vacations' => $vacations,
+                    'themes' => $themes,
+                    'hotels' => $hotels
         ]);
     }
-    
+
 }
-
-
-
