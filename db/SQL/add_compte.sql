4714fc3c9f489151055a52b3ba801c642b0c61e7
JérémyGrmt <59418445+JeremyGrmt@users.noreply.github.com>
2023-05-04 07:21:44 +0200

    Create add_compte.sql

---
+++ db/SQL/add_compte.sql
@@ -0,0 +1,6 @@
+
+use BDD_MDL_TEST;
+INSERT INTO `compte` (`id`, `inscription_id`, `email`, `roles`, `password`, `is_verified`, `numlicence`) VALUES
+(1, NULL, 'sem.Nulla.interdum@IntegermollisInteger.edu', '[\"ROLE_LICENCIE\", \"ROLE_USER\", \"ROLE_ADMIN\"]', '$2y$13$dCWwm.v7OeyOsQHg52CMrePlgiYHoBbWL/Bg9tLjVT6z7ND2lQjw2', 1, '16360514319'),
+(8, NULL, 'Duis.at@miacmattis.org', '[\"ROLE_LICENCIE\"]', '$2y$13$dDwBqcrEVj9hIKjOepl4KOd3VinZr2Y4EGIlmKTS2g/vMb.EL7giS', 1, '16790322264'),
+(11, NULL, 'ac@faucibusid.com', '[\"ROLE_LICENCIE\", \"ROLE_USER\"]', '$2y$13$rrQc85lIAM9Li7x31AhDjOyDyMaxbTSosMgoJTsCMExjUynfYISCu', 1, '16381117915');
